15a4c5d5231aae0f98169fc539ced79964414118
KarelSlgd <20213tn076@utez.edu.mx>
2024-08-14 16:52:32 -0600

    [FIX] paths

--- src/services/DetailsServices.js
+++ src/services/DetailsServices.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getOneCar = async (id) => {
   try {
     const response = await axios.get(
-      `https://fohggt0wwd.execute-api.us-east-1.amazonaws.com/Prod/get_one_car/?id_auto=${id}`
+      `https://syr3x8vt0g.execute-api.us-east-1.amazonaws.com/Prod/get_one_car/?id_auto=${id}`
     );    
     return response.data.data;
   } catch (error) {
--- src/services/HomeServices.js
+++ src/services/HomeServices.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getCars = async () => {
   try {
     const response = await axios.get(
-      "https://fohggt0wwd.execute-api.us-east-1.amazonaws.com/Prod/get_data_cars"
+      "https://syr3x8vt0g.execute-api.us-east-1.amazonaws.com/Prod/get_data_cars"
     );
     return response.data.data;
   } catch (error) {
